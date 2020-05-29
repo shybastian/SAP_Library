8ccce850f9b95f9f46ba738235f153f931ebf19e
shybastian <48442915+shybastian@users.noreply.github.com>
2020-05-29 18:26:59 +0300

    Updated fragments

--- webapp/controller/BookList.controller.js
+++ webapp/controller/BookList.controller.js
@@ -16,7 +16,6 @@ sap.ui.define([
                 TotalNumber:"",
                 AvailableNumber: 0
             }
-
         },
 
         onDeleteBook(oEvent){
@@ -28,45 +27,55 @@ sap.ui.define([
         onInsertBook(oEvent){
             if(!this.newBookDialog){
                 this.newBookDialog = sap.ui.xmlfragment("org.ubb.books.view.fragment.insert",this);
-                var oModel = new sap.ui.model.json.JSONModel();
-                this.newBookDialog.setModel(oModel);
-                this.newBookDialog.getModel().setData(this.book);
-            } else {
-                this.newBookDialog.open();
-            }        
+            }
+            this.resetBook();
+            var oModel = new sap.ui.model.json.JSONModel();
+            this.newBookDialog.setModel(oModel);
+            this.newBookDialog.getModel().setData(this.book);
+            this.newBookDialog.open();
         },
 
         onUpdateBook(oEvent){
             const aSelContext = this.byId("idBooksTable").getSelectedContexts();
             if (aSelContext.length != 0){
-                const oBook = aSelContext[0].getObject();
+                var oBook = aSelContext[0].getObject();
+                oBook = this.parseDatePublishedForUpdate(oBook);
+                this.book = oBook;
                 if(!this.updateBookDialog){
                     this.updateBookDialog = sap.ui.xmlfragment("org.ubb.books.view.fragment.update",this);
-                    var oModel = new sap.ui.model.json.JSONModel();
-                    this.updateBookDialog.setModel(oModel);
-                    this.updateBookDialog.getModel().setData(oBook);
-                    console.log(oBook);
-                    console.log("This is the data");
-                    console.log(oBook.DatePublished);
-                    const date = oBook.DatePublished;
-                    console.log(splitDate);
-                } else {
-                    this.updateBookDialog.open();
                 }
+                var oModel = new sap.ui.model.json.JSONModel();
+                this.updateBookDialog.setModel(oModel);
+                this.updateBookDialog.getModel().setData(this.book);
+                this.updateBookDialog.open();
             } else {
                 MessageToast.show("Select a Book to update it!");
             }
+        },
+
+        parseDatePublishedForCreate(oBook){
+            var dateString = oBook.DatePublished;
+            var dateFormat = sap.ui.core.format.DateFormat.getDateInstance({pattern : "dd/MM/yyyy" });
+            var TZOffsetMs = new Date(0).getTimezoneOffset()*60*1000;
+            var parsedDate = new Date(dateFormat.parse(dateString).getTime() - TZOffsetMs).getTime();
+            oBook.DatePublished ="\/Date(" + parsedDate + ")\/";
+            return oBook;
+        },
+
+        parseDatePublishedForUpdate(oBook){
+            var dateFormat = sap.ui.core.format.DateFormat.getDateInstance({pattern : "dd/MM/yyyy" }); 
+            var date = new Date(oBook.DatePublished);
+            var dateStr = dateFormat.format(date);
+            oBook.DatePublished = dateStr;
+            // oBook = this.parseDatePublishedForCreate(oBook);
+            return oBook;
         },
     
         saveBook(oEvent){
             if (this.book.TotalNumber < this.book.AvailableNumber){
                 MessageToast.show("Total Number must be greater than Available Number!");
             } else {
-                var dateString = this.book.DatePublished;
-                var dateFormat = sap.ui.core.format.DateFormat.getDateInstance({pattern : "dd/MM/yyyy" });
-                var TZOffsetMs = new Date(0).getTimezoneOffset()*60*1000;
-                var parsedDate = new Date(dateFormat.parse(dateString).getTime() - TZOffsetMs).getTime();
-                this.book.DatePublished ="\/Date(" + parsedDate + ")\/";
+                this.book = this.parseDatePublishedForCreate(this.book);
                 var oModel = this.getView().getModel();
                 oModel.create('/Books', this.book, {
                     success: function() {
@@ -81,13 +90,43 @@ sap.ui.define([
         },
 
         updateBook(oEvent){
+            debugger;
             if (this.book.TotalNumber < this.book.AvailableNumber){
-                MessageToast.show()
+                MessageToast.show("Total No. of Books must be greater than Available No. of Books!");
+            } else {
+                this.book = this.parseDatePublishedForCreate(this.book);
+                const aSelContexts = this.byId("idBooksTable").getSelectedContexts();
+                const sBookPath = aSelContexts[0].getPath();
+                var oModel = this.getView().getModel();
+                oModel.update(sBookPath, this.book, {
+                    success: function() {
+                        MessageToast.show("Book updated!");
+                    },
+                    error: function(){
+                        MessageToast.show("There was an error.");
+                    }
+                });
+                this.updateBookDialog.close();
             }
         },
 
         closeDialog(oEvent){
-            this.newBookDialog.close();
+            if (this.newBookDialog){
+                this.newBookDialog.close();
+            }
+            if (this.updateBookDialog){
+                this.updateBookDialog.close();
+            }
+        },
+
+        resetBook(){
+            this.book.ISBN = "";
+            this.book.Title = "";
+            this.book.Author = "";
+            this.book.DatePublished = "";
+            this.book.Language = "";
+            this.book.TotalNumber = 0;
+            this.book.AvailableNumber = 0;
         }
     });
 });
